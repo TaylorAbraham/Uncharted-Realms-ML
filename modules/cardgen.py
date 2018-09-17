eaddd7a43b8aebf77751fccb42251e1c05cc4914
RyanAbraham <ryan_ja@live.ca>
2018-09-17 00:58:38 -0400

    Add queue for holding generated cards

--- modules/cardgen.py
+++ modules/cardgen.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 import json
+from requests.exceptions import ConnectionError
+from urllib.error import URLError
 # Local libraries
 import modules.namegen as namegen
 import modules.imagegen as imagegen
@@ -11,6 +13,7 @@ csv_name = 'data/cards.csv'
 HP_vals = [1]*3 + [2]*5 + [3]*5 + [4]*4 + [5]*3 + [6]*2 + [7]*2 + [8] + [9] + [10]
 EFFs = ['Charge'] * 15 + ['Ward'] * 15
 
+# This is a class so that the current_id is static
 class IdGenerator:
     current_id = 0
     @classmethod
@@ -18,53 +21,58 @@ class IdGenerator:
         cls.current_id += 1
         return cls.current_id
 
-# Generates new cards
-def generate_cards(num_cards):
-    try:
-        num_cards = int(num_cards)
-    except ValueError:
-        return json.dumps({})
+# Generates a single new card
+def generate_card():
     card_dict = {'NAME': [], 'POW': [], 'HP': [], 'IMG': [], 'EFF': []}
     names = []
 
     # Generate base card attributes, to be costed later
-    for x in range(num_cards):
 
-        # If we ran out of names, generate a new batch of 15
-        if(names == []):
+    # If we ran out of names, generate a new batch of 15
+    if(names == []):
+        try:
             names = namegen.generate_names()
+        except ConnectionError:
+            print("Could not connect to name generation service")
 
+    if(names == []):
+        # If name service could not be connected to
+        name = "UNDEFINED"
+    else:
         name = random.choice(names)
-        card_dict['NAME'].append(name)
         names.remove(name)
+    card_dict['NAME'].append(name)
 
-        # HP is flat out a random int
-        HP = random.choice(HP_vals)
-        card_dict['HP'].append(HP)
+    # HP is flat out a random int
+    HP = random.choice(HP_vals)
+    card_dict['HP'].append(HP)
 
-        # Power is generated based on a gaussian distribution centred at the HP value
-        POW = int(round(HP + np.random.normal(0, HP/2, 1)[0], 0))
-        if(POW < 0):
-            POW = 0
-        card_dict['POW'].append(POW)
+    # Power is generated based on a gaussian distribution centred at the HP value
+    POW = int(round(HP + np.random.normal(0, HP/2, 1)[0], 0))
+    if(POW < 0):
+        POW = 0
+    card_dict['POW'].append(POW)
 
+    try:
         img = imagegen.generate_image_url(name)
-        card_dict['IMG'].append(img)
+    except URLError:
+        print("Could not connect to DeviantArt for image fetching")
+        img = ""
+    card_dict['IMG'].append(img)
 
-        card_dict['EFF'].append(random.choice(EFFs))
+    card_dict['EFF'].append(random.choice(EFFs))
 
     results = mlnetwork.predict_costs(card_dict)
 
     # Build the JSON for the response
-    response = {'cards': []}
     for index, card in results.iterrows():
-        response['cards'].append({})
-        response['cards'][index]['id'] = IdGenerator.generate_id()
-        response['cards'][index]['name'] = card['NAME']
-        response['cards'][index]['pow'] = card['POW']
-        response['cards'][index]['hp'] = card['HP']
-        response['cards'][index]['clk'] = card['CLK']
-        response['cards'][index]['eff'] = card['EFF']
-        response['cards'][index]['img'] = card['IMG']
+        generated_card = {}
+        generated_card['id'] = IdGenerator.generate_id()
+        generated_card['name'] = card['NAME']
+        generated_card['pow'] = card['POW']
+        generated_card['hp'] = card['HP']
+        generated_card['clk'] = card['CLK']
+        generated_card['eff'] = card['EFF']
+        generated_card['img'] = card['IMG']
 
-    return json.dumps(response)
+    return generated_card
--- server.py
+++ server.py
@@ -1,24 +1,57 @@
 from flask import Flask
+from flask import abort
 from flask_cors import CORS
 import modules.cardgen as cardgen
 import json
 import csv
+import threading
+import queue
 
 DEV_MODE = False
 
 app = Flask(__name__)
 CORS(app)
 
+QUEUE_SIZE = 30
+card_queue = queue.Queue(QUEUE_SIZE)
+generating_cards = True
+
+class GenerationThread(threading.Thread):
+    def run(self):
+        global generating_cards
+        global card_queue
+        while not card_queue.full():
+            card_queue.put(cardgen.generate_card())
+            print(card_queue.qsize())
+        print("Queue filled!")
+        generating_cards = False
+
 @app.route("/cards/generate/<qty>")
 def generate(qty):
-    response = cardgen.generate_cards(qty)
-    if(response == json.dumps({})):
-        return response, 400
-    return "" + response + ""
+    global generating_cards
+    print("Request received at /cards/generate/" + str(qty))
+    try:
+        qty = int(qty)
+    except ValueError:
+        abort(400)
+    if(qty > QUEUE_SIZE):
+        abort(400)
+
+    cards = []
+    for x in range(0, qty):
+        while card_queue.empty():
+            pass
+        cards.append(card_queue.get())
+    response = { 'cards': cards }
+    if(not generating_cards):
+        GenerationThread().start()
+        generating_cards = True
+    print("Response sent")
+    return "" + json.dumps(response) + ""
 
 if __name__ == "__main__":
-    uinput = ""
     if DEV_MODE:
+        uinput = ""
         while(uinput != "exit"):
             response = cardgen.generate_cards(3)
             uinput = input("Do you pick card #0, 1, or 2?\n")
@@ -31,5 +64,6 @@ if __name__ == "__main__":
                         f.writerow([None, card['POW'], card['HP'], card['CLK'], card['EFF']])
                     continue
             print("Invalid input")
+    GenerationThread().start()
     app.run()
 
